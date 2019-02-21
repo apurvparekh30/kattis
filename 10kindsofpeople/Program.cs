2baf6f3c29c0f19fff9f76a075e2714b5d0985a2
apurvparekh30 <apurvparekh30@gmail.com>
2019-02-20 20:54:22 -0500

    10kindsofpeople

---
+++ 10kindsofpeople/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _10kindsofpeople
+{
+    
+    class Program
+    {
+        static int r,c;
+        static char [][]grid;
+        static int []parent,rank;
+        static int n;
+        static int si,sj,di,dj;
+        static int Find(int i){
+            return (parent[i] == i ? i:parent[i] = Find(parent[i]));
+        }
+        static void Main(string[] args)
+        {
+            String []tokens = Console.ReadLine().Split();
+            r = int.Parse(tokens[0]);
+            c = int.Parse(tokens[1]);
+            grid = new char[r][];
+            parent = new int[r * c];
+            rank = new int[r * c];
+            for(int i=0;i<r;i++){
+                grid[i] = Console.ReadLine().ToCharArray();
+                for(int j=0;j<c;j++){
+                    parent[i * c + j] = i * c + j;
+                    int v = i - 1;
+                    if(v >= 0){
+                        if(grid[v][j] == grid[i][j]){
+                            int x = Find(v * c + j);
+                            int y = Find(i * c + j);
+                            if(rank[x] > rank[y]){
+                                parent[y] = x;
+                            }
+                            else {
+                                parent[x] = y;
+                                if(rank[x] == rank[y])
+                                    rank[y]++;
+                            }
+                        }
+                    }
+                    v = j - 1;
+                    if(v >= 0){
+                        if(grid[i][v] == grid[i][j]){
+                            int x = Find(i * c + v);
+                            int y = Find(i * c + j);
+                            if(rank[x] > rank[y]){
+                                parent[y] = x;
+                            }
+                            else {
+                                parent[x] = y;
+                                if(rank[x] == rank[y])
+                                    rank[y]++;
+                            }
+                        }
+                    }
+                }
+            }
+            n = int.Parse(Console.ReadLine());
+            while(n-- > 0){
+                tokens = Console.ReadLine().Split();
+                si = int.Parse(tokens[0]) - 1;
+                sj = int.Parse(tokens[1]) - 1;
+                di = int.Parse(tokens[2]) - 1;
+                dj = int.Parse(tokens[3]) - 1;
+                if(Find(si * c + sj) == Find(di * c + dj)){
+                    Console.WriteLine(grid[si][sj]=='1' ? "decimal" : "binary");
+                }
+                else
+                    Console.WriteLine("neither");
+            }
+        }
+    }
+}
---
+++ busyschedule/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace busyschedule
+{
+    class Program
+    {
+        static int n;
+        static Schedule []schedule;
+
+        class Schedule : IComparable{
+            string hour;
+            string minute;
+            string zone;
+            public Schedule(string hour,string minute,string zone){
+                this.hour = hour;
+                this.minute = minute;
+                this.zone = zone;
+            }
+            public int CompareTo(Object other){
+                Schedule o = (Schedule) other;
+                int h = int.Parse(this.hour)%12;
+                int hh = int.Parse(o.hour)%12;
+                int m = int.Parse(this.minute);
+                int mm = int.Parse(o.minute);
+                if(this.zone.Equals(o.zone)){
+                    if(h == hh)
+                        return m.CompareTo(mm);
+                    return h.CompareTo(hh);
+                }
+                return this.zone.CompareTo(o.zone);
+            }
+
+            public override string ToString(){
+                return hour+":"+minute+" "+zone;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while(true){
+                n = int.Parse(Console.ReadLine());
+                if(n==0)
+                    break;
+                schedule = new Schedule[n];
+        
+                for(int i=0;i<n;i++){
+                    string next = Console.ReadLine();
+                    string []tokens = next.Split(new char[]{':',' '});
+                    schedule[i] = new Schedule(tokens[0],tokens[1],tokens[2]);
+                }
+                Array.Sort(schedule);
+                foreach(Schedule s in schedule){
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
