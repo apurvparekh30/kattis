29dc3ce7e4c077cfba6e049825449c1ea3e9e529
apurvparekh30 <apurvparekh30@gmail.com>
2019-03-26 13:08:23 -0400

    raceday

--- raceday/Program.cs
+++ raceday/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace raceday
 {
     class Program
     {
-        static class user:IComparable <user> {
+        class user:IComparable <user> {
             string first,last,bib,s1,s2,finish;
             int r1,r2,r3;
             public string First {
@@ -18,7 +19,7 @@ namespace raceday
 
             public string Bib {
                 get { return bib; }
-                set { bit = value; }
+                set { bib = value; }
             }
             public string S1{
                 get { return s1; }
@@ -48,13 +49,73 @@ namespace raceday
                 get { return r3; }
                 set { r3 = value; }
             }        
-            public int compareTo(user other){
+            public int CompareTo(user other){
                 return last.CompareTo(other.last);
             }    
         }
         static void Main(string[] args)
         {
-            
+            while(true){
+                int n = int.Parse(Console.ReadLine());
+                if(n==0)
+                    break;
+                Dictionary<string,int> ht = new Dictionary<string,int>();
+                string []s1,s2,f;
+                user []us = new user[n];
+                for(int i=0;i<n;i++){
+                    us[i] = new user();
+                    string line = Console.ReadLine();
+                    string []tokens = line.Split();
+                    us[i].First = tokens[0];
+                    us[i].Last = tokens[1];
+                    us[i].Bib = tokens[2];
+                    ht[us[i].Bib] = i;
+                }
+                s1 = new string[n];
+                s2 = new string[n];
+                f = new string[n];
+                int si=0,sj=0,sk=0;
+                for(int i = 0; i < 3 * n; i++){
+                    string line = Console.ReadLine();
+                    string[] tokens = line.Split();
+                    string bib = tokens[0];
+                    string ss = tokens[1];
+                    string dd = tokens[2];
+                    if(ss.Equals("S1")){
+                        us[ht[bib]].S1 = dd;
+                        s1[si++] = dd;
+                    }
+                    else if(ss.Equals("S2")){
+                        us[ht[bib]].S2 = dd;
+                        s2[sj++] = dd;
+                    }
+                    else {
+                        us[ht[bib]].Finish = dd;
+                        f[sk++] = dd;
+                    }
+                }
+                Array.Sort(us);
+                Array.Sort(s1);
+                Array.Sort(s2);
+                Array.Sort(f);
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<n;j++){
+                        if(s1[j].Equals(us[i].S1)){
+                            us[i].R1 = i+1;
+                        }
+                    }
+                    for(int j=0;j<n;j++){
+                        if(s2[j].Equals(us[i].S2)){
+                            us[i].R2 = i+1;
+                        }
+                    }
+                    for(int j=0;j<n;j++){
+                        if(f[j].Equals(us[i].Finish)){
+                            us[i].R3 = i+1;
+                        }
+                    }
+                }
+            }
         }
     }
 }
